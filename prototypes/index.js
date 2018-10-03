ba3ea8237d5eb2da430664c75d532ae79b834f9b
Derek Hanciles <drhanciles@icloud.com>
2018-10-03 10:58:22 -0600

    Completed work from Tuesday

--- prototypes/index.js
+++ prototypes/index.js
@@ -16,11 +16,17 @@ const turingPrompts = {
     //  { name: 'Robbie', studentCount: 18 }
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = instructors.map(instructor => {
+      let newInstructor = {name: instructor.name}
+      let matchingMod = cohorts.find(cohort => cohort.module === instructor.module)
+      newInstructor.studentCount = matchingMod.studentCount
+      return newInstructor
+    })
     return result;
 
     // Annotation:
     // Write your annotation here as a comment
+    // We are given two arrays, and we want to return an array of the same length. We are going to reach for a map because each element in our instructor array has a key of instructor. We will interate over our instuctor array first. 
   },
 
   studentsPerInstructor() {
@@ -30,11 +36,18 @@ const turingPrompts = {
     // cohort1804: 10.5
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cohorts.reduce((obj, cohort) => {
+    let key = `cohort${cohort.cohort}`
+    let instructorCount = instructors.filter(intstructor => instructor.module === cohort.module).length
+    let studentsPerInstructor = cohort.studentCount / instructorCount
+    
+    obj[key] = studentsPerInstructor
+    return obj
+    }, {}); 
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+  //  We are given two arrays, and we want to return the an object of object where our keys are are the cohort and value is students per instructor. We are going to reach for a reduce because of it's flexibility, with it being able to divide our total amount of students by how many instructors teach each module. To do this I will first iterate over our cohorts to create our key. Then I will iterate over our instuctors and return an array where each element is our instuctors that match that module, and I will use the length of that array, and divide our cohort student count by the length. 
   },
 
   modulesPerTeacher() {
@@ -47,11 +60,24 @@ const turingPrompts = {
     //   Pam: [2, 4]
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = instructors.reduce((obj, instructor) => {
+    let name = instructor.name
+    obj[name] = []
+    instructor.teaches.forEach(lesson => {
+      cohorts.forEach(cohort => {
+        cohort.curriculum.forEach(subject => {
+          if (lesson === subject && !obj[name].includes(cohort.module)) {
+            obj[name].push(cohort.module)
+          }
+        })
+      })
+    })
+    return obj
+    }, {}); 
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // We are given two arrays, and we want to return an object. In this object our keys will be each instructor and it's value will be the modules each instructor can teach. To do this I will reach for a reduce, because of it's flexibility it will return an object where we can dynamically create our key value pairs. We first want to iterate over our instructors set each name as our key, then we will iterate over our cohort and find the modules our instructors can teach based off of their what they teach and each cohorts curriculum. 
   },
 
   curriculumPerTeacher() {
@@ -64,11 +90,25 @@ const turingPrompts = {
     //   recursion: [ 'Pam', 'Leta' ]
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cohorts.reduce((obj, cohort) => {
+      cohort.curriculum.forEach(subject => {
+        if (!obj[subject]) {
+          obj[subject] = []
+        }
+        instructors.forEach(instructor => {
+          instructor.teaches.forEach(lesson => {
+            if (!obj[subject].includes(instructor.name) && lesson === subject) {
+              obj[subject].push(instructor.name)
+            } 
+          })
+        })
+      })
+      return obj
+      }, {}); 
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // We are given two arrays and we want to return an object. Our keys in our object will be our curriculum and their value will be an array of instructors that can teach the curriculum. To create this object we will use a reduce due to it's flexibility. We first will iterate over each cohorts curriculum to create our keys, then we will iterate over our instructors to then add the names of our instructors who can teach that curriculum to our array
   }
 };
 
@@ -190,11 +230,18 @@ const cakePrompts = {
     // every cake in the dataset e.g.
     // ['dutch process cocoa', 'toasted sugar', 'smoked sea salt', 'berries', ..etc]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.reduce((arr, cake) => {
+      cake.toppings.forEach(topping => {
+        if(!arr.includes(topping)){
+          arr.push(topping)
+        }
+      })
+      return arr
+    }, []);
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // We are given an array. We want to return an array. I will reach for a reduce, because of its flexibility. The values I want are stored in an array. I would have to use a forEach method to iterate through those elements and add them to our array. 
   },
 
   groceryList() {
@@ -208,11 +255,21 @@ const cakePrompts = {
     //    ...etc
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.reduce((obj, cake) => {
+      cake.toppings.forEach(topping => {
+        if (!obj[topping]) {
+          obj[topping] = 1
+        } else {
+          obj[topping]++
+        }
+      }); 
+      return obj
+    }, {}); 
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+  //  We are given an array. We want to return an object where our keys are each topping in our toppings array. The value of each key will be the number of times the unique toppings appear in our different toppings array. I am going to reach for a reduce method to create this object,  because of its flexibility will allow us to dynamically create each key. To find the value for our key we will have to use forEach method, because will allow me to create our creat through each iteration of our topping, but I can also check if that key has already been created if it has we want to increment our current key value each time it has been found.
+   
   },
 
   stockPerCake() {
@@ -224,22 +281,27 @@ const cakePrompts = {
     //    ..etc
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.map(cake => {
+      return {flavor: cake.cakeFlavor, inStock: cake.inStock}
+    });
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // We are given an Array. We want to return an Array, where each element in our array is an object. The objects will have two key value pairs. Our first key value pair will match each cake flavor we have so we will use the .map() method to return our cakeFlavor as our first value, and it will also return our second value which is the inStock value of each object
   },
 
   totalInventory() {
     // Return the total amout of cakes in stock e.g.
     // 59
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.reduce((sum, cake) => {
+      sum += cake.inStock
+      return sum
+    }, 0);
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // We are given an Array. In our array each element is an Object. In each object there is a key 'inStock', and it's value is a number. We want to return the sum off all of these key value pairs in our object. To do this I will reach for using a .reduce(), because of it's flexibility, it can return this single value to us
   },
 
   onlyInStock() {
@@ -262,7 +324,12 @@ const cakePrompts = {
     // },
     // ..etc
     // ]
+    const result = cakes.filter(cake => cake.inStock)
+    return result;
   }
+    // Annotation: 
+    // Receiving an array of cakes, and I want to return a subset of that array, so I am going to reach for a filter. Any cake that has 0 as it's value for inStock will not be returned, because 0 is falsy. 
+
 };
 
 
